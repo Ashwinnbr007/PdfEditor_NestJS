b1bba35b9d457efa8d490f1889c2427afbbe4587
Ashwinnbr007 <ashwinnambiarbusiness@gmail.com>
2023-07-14 15:55:04 +0530

    Frontend can talk to backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
 
 function App() {
+
+  async function handleLoad() {
+    const url = process.env.REACT_APP_API_URL;
+    console.log(url)
+    let res = await fetch(url)
+    let response = await res.json()
+    console.log(response)
+  }
+
   return (
-    <div className="App">
-      hello world
+    <div className='main'>
+      <span>
+        <button onClick={handleLoad}>
+          load PDF
+        </button>
+        <button>
+          save PDF
+        </button>
+      </span>
     </div>
   );
 }
