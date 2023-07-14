8cb0cdeab7d69ea9804ead0b46d40b1403720941
Ashwinnbr007 <ashwinnambiarbusiness@gmail.com>
2023-07-14 19:26:59 +0530

    renaming endpoint

--- backend/src/file/file.controller.ts
+++ backend/src/file/file.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { FileService } from './file.service';
 
-@Controller('api')
+@Controller('file')
 export class FileController {
   constructor(private readonly fileService: FileService) {}
 
